494990c2a4b70c6d22b346a9fc290ff6ff01faee
Victor <119737325+MikLomonosov@users.noreply.github.com>
2023-08-18 16:08:28 +0300

    host changed DataBaseWorker.java

--- src/main/java/com/example/TestMockv2/DAL/DataBaseWorker.java
+++ src/main/java/com/example/TestMockv2/DAL/DataBaseWorker.java
@@ -10,7 +10,7 @@ public class DataBaseWorker {
 
     //region Fields
 
-    private String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+    private String dbUrl = "jdbc:postgresql://0.0.0.0:5432/postgres";
     private String dbUser = "postgres";
     private String dbPassword = "postgres";
     private String authenticateTable = "authenticate";
